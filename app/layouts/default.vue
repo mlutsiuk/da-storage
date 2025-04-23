8a425021484100c81c85c201e7610959c0b2805b
mlutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2025-04-23 17:22:06 +0300

    feat: Update default layout

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -2,7 +2,7 @@
   <div class="flex min-h-screen flex-col items-stretch">
     <AppHeader />
 
-    <div class="grow p-6">
+    <div class="grow p-6 max-w-[1440px] mx-auto w-full">
       <slot />
     </div>
   </div>
