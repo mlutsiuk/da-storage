f369e0f55350337be89cc012d2ada5f49b1973d0
mlutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2025-04-23 17:21:56 +0300

    feat: Add logout page

---
+++ app/pages/auth/logout.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+definePageMeta({
+  middleware: 'auth',
+  layout: 'hero'
+})
+
+onMounted(() => {
+  useAuthStore().logout()
+
+  useToast().add({
+    title: 'You have been logged out',
+    description: 'See you soon!'
+  })
+
+  useRouter().push({ name: 'auth-login' })
+})
+</script>
+
+<template>
+  <UIcon
+    name="i-svg-spinners-180-ring"
+    class="text-primary size-10"
+    dynamic
+  />
+</template>
