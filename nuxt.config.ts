a8c74b0c9cda43089ee62db1f1a7ef2e653227ce
mlutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2025-04-26 03:48:00 +0300

    feat: Configure PWA

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -41,6 +41,31 @@ export default defineNuxtConfig({
 
   ssr: false,
 
+  pwa: {
+    strategies: 'generateSW',
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'DA Storage',
+      short_name: 'DA Storage',
+      description: 'DA Storage - App to manage stockpiles',
+      lang: 'en',
+      start_url: '/',
+      display: 'standalone',
+      theme_color: '#0E1927',
+      background_color: '#0E1927',
+    },
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,png,svg,ico}']
+    },
+    injectManifest: {
+      globPatterns: ['**/*.{js,css,html,png,svg,ico}']
+    },
+    client: {
+      installPrompt: true,
+      periodicSyncForUpdates: 3600
+    }
+  },
+
   runtimeConfig: {
     jwt: {
       secret: '',
